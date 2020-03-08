f9fca574fdef15a040e7e964dab8d953558b0f2b
Leonardo Robol <leo@robol.it>
2020-03-08 10:26:46 +0100

    Tentative support for Google Meet.

--- background.js
+++ background.js
@@ -1,5 +1,6 @@
 chrome.runtime.onInstalled.addListener(function() {
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+      // Rule for matching at teams.microsoft.com
       chrome.declarativeContent.onPageChanged.addRules([{
         conditions: [new chrome.declarativeContent.PageStateMatcher({
           pageUrl: {hostEquals: 'teams.microsoft.com'},
@@ -7,24 +8,31 @@ chrome.runtime.onInstalled.addListener(function() {
         ],
         actions: [new chrome.declarativeContent.ShowPageAction()]
       }]);
+
+      // Rule for matching at meet.google.com
+      chrome.declarativeContent.onPageChanged.addRules([{
+        conditions: [new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: {hostEquals: 'meet.google.com'},
+        })
+        ],
+        actions: [new chrome.declarativeContent.ShowPageAction()]
+      }]);
     });
 
     chrome.runtime.onMessage.addListener(
-      function(request, sender, sendResponse) {
-        console.log(sender.tab ?
-                    "from a content script:" + sender.tab.url :
-                    "from the extension");
-        if (request.action == "download") {
-            let msg = request.message;
+        function(request, sender, sendResponse) {
+            if (request.action == "download") {
+                let msg = request.message;
 
-	        var blob = new Blob([msg], {type: "text/plain"});
-	        var url = URL.createObjectURL(blob);
-	        chrome.downloads.download({
-          		url: url,
-          		filename: 'participants.txt'
-	        });
+	              var blob = new Blob([msg], {type: "text/plain"});
+	              var url = URL.createObjectURL(blob);
+	              chrome.downloads.download({
+              		  url: url,
+              		  filename: 'participants.txt'
+	              });
 
-            sendResponse({ status: 'completed' });
+                sendResponse({ status: 'completed' });
+            }
         }
-      });
-  });
+    );
+});
--- contentscript.js
+++ contentscript.js
@@ -2,23 +2,34 @@ function savepart_main() {
 
     let participants_list = "";
 
+    // This code works for Microsoft Teams
     Array.from(document.getElementsByTagName('li')).forEach(function (ll) {
         let datatid = ll.getAttribute('data-tid');
         if (datatid != null && datatid.includes('participantsInCall')) {
 	        var name = datatid.substr(19); // Skip the prefix. 
-	        console.log('Found partecipants: ' + name);
+	        console.log('Found partecipant: ' + name);
 	        participants_list = participants_list + name + "\n";
         }
     });
 
-	// alert("Partecipanti al corso:\n\n" + participants_list);
-    chrome.runtime.sendMessage({ action: 'download', 
-                                 message: "Participants:\n\n" + participants_list},
-                               function (response) {
-                                   if (response.status != 'completed') {
-                                       console.log('Download fallito');
-                                   }                                   
-                               });
+    // And this works for Google Meets, assuming the class names are correct and not
+    // linked to my specific session.
+    Array.from(document.getElementsByClassName('cS7aqe NkoVdd')).forEach(function (div) {
+        var name = div.innerHTML;
+        console.log('Found partecipant: ' + name);
+        participants_list = participants_list + name + "\n";
+    });
+
+    chrome.runtime.sendMessage(
+      {
+         action: 'download',
+         message: "Participants:\n\n" + participants_list
+      },
+      function (response) {
+          if (response.status != 'completed') {
+              console.log('Download fallito');
+          }
+      });
 
 
 }
